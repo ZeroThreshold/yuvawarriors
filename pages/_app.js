20af266733cde99c5334580cf67ed8e759cf58e6
roverflow <roverflow5@gmail.com>
2024-05-01 13:09:30 +0530

    form submission

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 import TagManager from "react-gtm-module";
 import "styles/style.scss";
 import "styles/global.css";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = ({ Component, pageProps }) => {
   // default theme setup
